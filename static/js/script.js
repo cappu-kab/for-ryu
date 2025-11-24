7bd1730c44cc60abae0564116adbed0c00e5e86c
cappu-kab <charlotte.kamoshita00@gmail.com>
2025-11-24 21:16:43 +0700

    ryu

---
+++ static/js/script.js
@@ -0,0 +1,27 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Page loaded!");
+
+
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
+const acceptedAnswers = [
+  "Old With You",
+  "old with you",
+  "OLD WITH YOU",
+  "Old with you",
+  "old With You",
+  "OldWithYou",
+  "oldwithyou",
+  "OLDWITHYOU",
+  "Oldwithyou",
+  "oldWithYou",
+  "old_With_You",
+  "Old_With_You",
+  "oldwith you",
+  "old withYou",
+  "Old withYou",
+  "OldWithyou",
+];
+
